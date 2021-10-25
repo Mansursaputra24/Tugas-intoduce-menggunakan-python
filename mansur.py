cf85f369a4a61e0c21a6e7fdf3f5a54a8db9d3ae
Mansursaputra24 <92736834+Mansursaputra24@users.noreply.github.com>
2021-10-25 14:55:26 +0700

    Add files via upload

---
+++ mansur.py
@@ -0,0 +1,14 @@
+#Tugas pembuatan program memperkenalkan atau intoduce sederhana menggunakan python
+#nama file mansur.py
+
+print ('Python adalah salah satu bahasa pemrograman yang dapat melakukan eksekusi sejumlah instruksi multi guna secara langsung (interpretatif) dengan metode orientasi objek. Python adalah bahasa pemrograman yang paling mudah dipahami. Python dibuat oleh programmer Belanda bernama Guido Van Rossum')
+print ('beberapa kekurangan dan kelebihan dari python ini adalah: ')
+print ('Kekurangan Python : ')
+print ('1. Pyton tidak secepat atau se efisien bahasa C, yang cenderung lebih statis')
+print ('2. Python cukup buruk untuk mengembangkan platform mobile ')
+print ('3. Python merupakan intepreter, sehingga bukanlah alat bantu terbaik untuk pengantar komponen kerja kritis ')
+print ('Kelebihan Python : ')
+print ('1. Python adalah bahasa pemrograman yang populer ')
+print ('2. Python merupakan bahasa yang multi fungsi ')
+print ('3. Penulisan kode lebih efisien dibandingkan dengan bahasa yang laiinya, seperti C++,C, Java ')
+
